8e5c9d71ef598ceb8715ed8a180a080a786491e5
Andrew Silver <andrew.silver0@gmail.com>
2015-06-30 20:58:21 +1000

    Added scroll resizing

--- whiteboard.js
+++ whiteboard.js
@@ -1,4 +1,18 @@
 var canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
 
-canvas.
+canvas.style.position = "absolute";
+canvas.style.top = 0;
+canvas.style.left = 0;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.style.margin = 0;
+canvas.style.padding = 0;
+
+document.addEventListener('scroll', function(evt) {
+	if (canvas.height < window.innerHeight + evt.pageY)
+		canvas.height = window.innerHeight + evt.pageY;
+	if (canvas.width < window.innerWidth + evt.pageX)
+		canvas.width = window.innerWidth + evt.pageX;
+});
