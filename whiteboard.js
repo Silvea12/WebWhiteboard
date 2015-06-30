5fba539cbf2d39b6b738ab6b19a75449f20b30e6
Andrew Silver <andrew.silver0@gmail.com>
2015-06-30 21:31:32 +1000

    Added cross-browser compatability

--- whiteboard.js
+++ whiteboard.js
@@ -1,18 +1,44 @@
 var canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
-
-canvas.style.position = "absolute";
+canvas.style.position = 'absolute';
 canvas.style.top = 0;
 canvas.style.left = 0;
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 canvas.style.margin = 0;
 canvas.style.padding = 0;
-
+canvas.style['z-index'] = 10000;
+ctx.lineStyle = '#000000';
+ctx.fillStyle = '#000000';
+var clicking = false;
+var needsRefresh = false;
+canvas.addEventListener('mousedown', function(evt) {
+	ctx.moveTo(evt.pageX, evt.pageY);
+	clicking = true;
+});
+canvas.addEventListener('mouseup', function() {
+	clicking = false;
+});
+var refresh = function() {
+	if (needsRefresh)
+	{
+		ctx.stroke();
+		needsRefresh = false;
+	}
+	requestAnimationFrame(refresh);
+};
+refresh();
+canvas.addEventListener('mousemove', function(evt) {
+	if (clicking)
+	{
+		ctx.lineTo(evt.pageX, evt.pageY);
+		needsRefresh = true;
+	}
+});
 document.addEventListener('scroll', function(evt) {
-	if (canvas.height < window.innerHeight + evt.pageY)
-		canvas.height = window.innerHeight + evt.pageY;
-	if (canvas.width < window.innerWidth + evt.pageX)
-		canvas.width = window.innerWidth + evt.pageX;
+	if (canvas.height < window.innerHeight + window.pageYOffset)
+		canvas.height = window.innerHeight + window.pageYOffset;
+	if (canvas.width < window.innerWidth + window.pageXOffset)
+		canvas.width = window.innerWidth + window.pageXOffset;
 });
